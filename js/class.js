0528e60bf84de6d8b12a0b9bd80e6e108804712a
velvet_vixen <hei.karenandriantasy@gmail.com>
2025-09-15 09:30:16 +0400

    feat: stage

--- js/class.js
+++ js/class.js
@@ -6,3 +6,45 @@ menuBtn.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
 
+
+// project filter
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tab-btn");
+  const contents = document.querySelectorAll(".tab-content");
+
+  // Fonction pour afficher le bon onglet
+  function showTab(tabName) {
+      contents.forEach(c => {
+          if (tabName === "tous") {
+              // Afficher toutes les sections si "Tous" est sélectionné
+              c.classList.remove("hidden");
+          } else if (c.id === tabName) {
+              c.classList.remove("hidden");
+          } else {
+              c.classList.add("hidden");
+          }
+      });
+
+      // Mettre à jour la couleur des onglets
+      tabs.forEach(t => {
+        if (t.dataset.tab === tabName) {
+            t.style.color = "var(--text-white)";
+            t.style.backgroundColor = "var(--bg-red)";
+        } else {
+            t.style.color = "black";
+            t.style.backgroundColor = "#E5E5E5"; 
+        }
+    });
+    
+  }
+
+  // Activation au clic
+  tabs.forEach(tab => {
+      tab.addEventListener("click", () => {
+          showTab(tab.dataset.tab);
+      });
+  });
+
+  // Affichage par défaut : Formation
+  showTab("formation");
+});
